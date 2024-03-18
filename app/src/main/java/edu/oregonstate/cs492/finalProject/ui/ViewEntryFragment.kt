480f56fe68f6327a308c2efa73f9be79290eb92e
Julia Yang <juliayang365@gmail.com>
2024-03-18 16:01:30 -0700

    added sample data, need to set up recycler view and database still

--- app/src/main/java/edu/oregonstate/cs492/finalProject/ui/ViewEntryFragment.kt
+++ app/src/main/java/edu/oregonstate/cs492/finalProject/ui/ViewEntryFragment.kt
@@ -3,28 +3,28 @@ package edu.oregonstate.cs492.finalProject.ui
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import edu.oregonstate.cs492.finalProject.R
 
 class ViewEntryFragment : Fragment(R.layout.fragment_journal) {
-//    private lateinit var journalEntryTextView: EditText
     private lateinit var addButton: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        journalEntryTextView = view.findViewById(R.id.journal_text_box) as EditText
         addButton = view.findViewById(R.id.add_entry_button)
 
         addButton.setOnClickListener {
             val direction = ViewEntryFragmentDirections.addJournalEntries()
             findNavController().navigate(direction)
-
         }
-    }
 
-//    fun displayJournalEntry(journalEntry: String) {
-//        journalEntryTextView.setText(journalEntry)
-//    }
+        val titleTextView1 = view.findViewById<TextView>(R.id.title_tv)
+        val entryTextView1 = view.findViewById<TextView>(R.id.journal_entry_tv)
+
+        titleTextView1.text = "Sample Entry 1"
+        entryTextView1.text = "Random sample text for entry 1"
+    }
 }
