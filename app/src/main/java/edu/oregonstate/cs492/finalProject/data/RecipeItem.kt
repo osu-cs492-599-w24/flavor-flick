1653e8e2230c2f91e9212227e58901f38bbfb330
Jonathan Pham <phamjonathan364@outlook.com>
2024-03-18 03:07:01 -0700

    Started on swipe feature

---
+++ app/src/main/java/edu/oregonstate/cs492/finalProject/data/RecipeItem.kt
@@ -0,0 +1,10 @@
+package edu.oregonstate.cs492.finalProject.data
+
+data class RecipeItem(
+    val strMeal: String,
+    val strMealThumb: String,
+    val strCategory: String,
+    val strarea: String,
+    val strYoutube: String,
+    val strSource: String
+)
