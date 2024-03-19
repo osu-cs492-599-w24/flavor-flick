963845a0c7af5e17759bf5d1128af3500f9ad7c8
oliverflood <oliveroldrich@gmail.com>
2024-03-18 17:08:28 -0700

    edit RecipeItem JSON names

--- app/src/main/java/edu/oregonstate/cs492/finalProject/data/RecipeItem.kt
+++ app/src/main/java/edu/oregonstate/cs492/finalProject/data/RecipeItem.kt
@@ -1,10 +1,18 @@
 package edu.oregonstate.cs492.finalProject.data
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * This class is used to help parse the JSON recipe data returned by the TheMealDB API's
+ * "Lookup a single random meal" endpoint.
+ */
+@JsonClass(generateAdapter = true)
 data class RecipeItem(
-    val strMeal: String,
-    val strMealThumb: String,
-    val strCategory: String,
-    val strarea: String,
-    val strYoutube: String,
-    val strSource: String
+    @Json(name = "name") val strMeal: String,
+    @Json(name = "image") val strMealThumb: String,
+    @Json(name = "category")  val strCategory: String,
+    @Json(name = "region") val strArea: String,
+    @Json(name = "videoLink") val strYoutube: String,
+    @Json(name = "recipeLink") val strSource: String
 )
