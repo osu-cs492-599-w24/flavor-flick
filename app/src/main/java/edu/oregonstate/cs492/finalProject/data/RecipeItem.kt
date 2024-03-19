0f3a417844b9753fa211170df6426c92d798b5eb
oliverflood <oliveroldrich@gmail.com>
2024-03-18 17:51:04 -0700

    added new data class for API response parsing, fixed RecipeItem data class

--- app/src/main/java/edu/oregonstate/cs492/finalProject/data/RecipeItem.kt
+++ app/src/main/java/edu/oregonstate/cs492/finalProject/data/RecipeItem.kt
@@ -6,13 +6,14 @@ import com.squareup.moshi.JsonClass
 /**
  * This class is used to help parse the JSON recipe data returned by the MealDB API's
  * "Lookup a single random meal" endpoint.
+ * This class represents a single recipe in the list of returned meals.
  */
 @JsonClass(generateAdapter = true)
 data class RecipeItem(
-    @Json(name = "name") val strMeal: String,
-    @Json(name = "image") val strMealThumb: String,
-    @Json(name = "category")  val strCategory: String,
-    @Json(name = "region") val strArea: String,
-    @Json(name = "videoLink") val strYoutube: String,
-    @Json(name = "recipeLink") val strSource: String
+    @Json(name = "strMeal") val name: String,
+    @Json(name = "strMealThumb") val image: String,
+    @Json(name = "strCategory") val category: String,
+    @Json(name = "strArea") val region: String,
+    @Json(name = "strYoutube") val videoLink: String,
+    @Json(name = "strSource") val recipeLink: String
 )
